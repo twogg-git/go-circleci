7029675f48085b4a3d63e7e7ff2d59f0c7680898
twogg-git <cruz.catherin@gmail.com>
2019-06-24 03:24:38 -0500

    Addind code

---
+++ main.go
@@ -0,0 +1,82 @@
+package main
+
+import (
+	"fmt"
+	"go-circleci-strings/validations"
+	"html/template"
+	"net"
+	"net/http"
+	"time"
+)
+
+const VERSION = "v1.0"
+const PORT = "8082"
+
+type Endpoint struct {
+	Path   string
+	Type   string
+	Active bool
+}
+
+type TemplateData struct {
+	ServerIP  string
+	Version   string
+	Deployed  string
+	Port      string
+	Endpoints []Endpoint
+}
+
+const FORMAT = "2006-01-02 15:04:05"
+
+func getServerIP() string {
+	conn, err := net.Dial("udp", "8.8.8.8:80")
+	if err != nil {
+		panic(err)
+	}
+	defer conn.Close()
+	return conn.LocalAddr().(*net.UDPAddr).IP.String()
+}
+
+func main() {
+	tmpl := template.Must(template.ParseFiles("temp.html"))
+
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		data := TemplateData{
+			ServerIP: getServerIP(),
+			Version:  VERSION,
+			Deployed: time.Now().Local().Format(FORMAT),
+			Port:     PORT,
+			Endpoints: []Endpoint{
+				{Path: "/flip", Type: "GET", Active: true},
+				{Path: "/time", Type: "GET", Active: true},
+				{Path: "/version", Type: "GET", Active: false},
+			},
+		}
+		tmpl.Execute(w, data)
+	})
+
+	http.HandleFunc("/time", func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Set("Content-Type", "text/html; charset=utf-8")
+		w.Header().Set("Access-Control-Allow-Origin", "*")
+		current := time.Now().Local()
+		fmt.Fprintf(w, current.Format(FORMAT))
+	})
+
+	http.HandleFunc("/version", func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Set("Content-Type", "text/html; charset=utf-8")
+		w.Header().Set("Access-Control-Allow-Origin", "*")
+		fmt.Fprintf(w, VERSION)
+	})
+
+	http.HandleFunc("/flip", func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Set("Content-Type", "text/html; charset=utf-8")
+		w.Header().Set("Access-Control-Allow-Origin", "*")
+		textToFlip := r.FormValue("text")
+		fmt.Fprintf(w, validations.Flip(textToFlip))
+	})
+
+	if err := http.ListenAndServe(":"+PORT, nil); err != nil {
+		panic(err)
+	}
+
+}
